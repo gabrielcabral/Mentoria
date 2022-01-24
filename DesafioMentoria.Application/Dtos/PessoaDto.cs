d5d034b7e5eca2fec6048a91439efe0247099887
Gabriel Cabral <cabraldealmeida@gmail.com>
2022-01-24 08:09:07 -0300

    Mentoria

---
+++ DesafioMentoria.Application/Dtos/PessoaDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioMentoria.Application.Dtos
+{
+    public class PessoaDto
+    {
+        public int Id { get; set; }
+        public string Nome  { get; set; }
+        public string Cpf { get; set; }
+        public string DataNascimento { get; set; }
+        public int Sexo { get; set; }
+        public bool Ativo { get; set; }
+
+ 
+    }
+}
---
+++ DesafioMentoria.Application/Mappers/MapperPessoa.cs
@@ -0,0 +1,59 @@
+﻿using DesafioMentoria.Application.Dtos;
+using DesafioMentoria.Application.Interfaces.Mappers;
+using DesafioMentoria.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DesafioMentoria.Application.Mappers
+{
+    public class MapperPessoa : IMapperPessoa
+    {
+        Regex rgx = new Regex("[^0-9]");
+
+        public Pessoa MapperDtoToEntity(PessoaDto pessoaDto)
+        {
+
+            var pessoa = new Pessoa()
+            {
+                Id = pessoaDto.Id,
+                Nome = pessoaDto.Nome,
+                Cpf = rgx.Replace(pessoaDto.Cpf, ""),
+                DataNascimento = Convert.ToDateTime(pessoaDto.DataNascimento),
+                Sexo = pessoaDto.Sexo,
+                Ativo = pessoaDto.Ativo
+
+            };
+            return pessoa;
+        }
+
+        public PessoaDto MapperEntityToDto(Pessoa pessoa)
+        {
+            var pessoaDto = new PessoaDto()
+            {
+                Id = pessoa.Id,
+                Nome = pessoa.Nome,
+                Cpf = pessoa.Cpf,
+                DataNascimento = pessoa.DataNascimento.ToString(),
+                Sexo = pessoa.Sexo,
+                Ativo = pessoa.Ativo
+            };
+            return pessoaDto;
+        }
+
+        public IEnumerable<PessoaDto> MapperListPessoaDto(IEnumerable<Pessoa> pessoas)
+        {
+            var dto = pessoas.Select(c => new PessoaDto
+            {
+               Id = c.Id,
+               Nome = c.Nome,
+               Sexo= c.Sexo,    
+               Cpf= c.Cpf,
+               DataNascimento= c.DataNascimento.ToString(),   
+               Ativo= c.Ativo
+            });
+            return dto;
+        }
+    }
+}
